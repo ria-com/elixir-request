d84d2d8ca43b871ec3b3019051b48f2573862009
devprogramer <fesenko.victor@gmail.com>
2018-08-29 11:13:32 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ria.Request.Mixfile do
   def project do
     [
       app: :ria_request,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
